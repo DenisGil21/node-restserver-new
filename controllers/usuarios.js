4c51de441cda6ebbdea340a1bd63d6eceddd3f0b
Denis Gil <denisgilsil.21@gmail.com>
2021-03-24 14:20:31 -0600

    RestServer básico

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,20 +1,37 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = 'undefined', apikey, page = 1, limit } = req.query;
+
     res.json({
-        msg: 'get-controlador'
+        msg: 'get-controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
     });
 }
 
 const usuariosPut = (req, res) => {
+
+    const id = req.params.id;
+
     res.status(401).json({
-        msg: 'put'
+        msg: 'put',
+        id
     });
 }
 
 const usuariosPost = (req, res) => {
+
+    const { nombre, edad } = req.body;
+
     res.status(201).json({
-        msg: 'post'
+        msg: 'post',
+        nombre,
+        edad,
     });
 }
 
