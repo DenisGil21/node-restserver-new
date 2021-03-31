688b5bdb3bbb17dd29b3cd284e92650f1ea9cc00
Denis Gil <denisgilsil.21@gmail.com>
2021-03-31 11:27:11 -0600

    Usuario CRUD listo 31-03-21

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,43 +1,69 @@
 const { response, request } = require('express');
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
 
-    const { q, nombre = 'undefined', apikey, page = 1, limit } = req.query;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
 
     res.json({
-        msg: 'get-controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+        total,
+        usuarios
     });
 }
 
-const usuariosPut = (req, res) => {
+const usuariosPut = async(req, res = response) => {
 
-    const id = req.params.id;
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
 
-    res.status(401).json({
-        msg: 'put',
-        id
-    });
+    // TODO validar contra base de dattos
+    if (password) {
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+    res.status(401).json(usuario);
 }
 
-const usuariosPost = (req, res) => {
+const usuariosPost = async(req, res = response) => {
 
-    const { nombre, edad } = req.body;
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //Guardar en DB
+    await usuario.save();
 
     res.status(201).json({
-        msg: 'post',
-        nombre,
-        edad,
+        usuario,
     });
 }
 
-const usuariosDelete = (req, res) => {
+const usuariosDelete = async(req, res = response) => {
+    const { id } = req.params;
+
+    //Fisicamente se borra
+    // const usuario = await Usuario.findByIdAndDelete(id);
+    const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+
     res.json({
-        msg: 'delete'
+        msg: 'delete',
+        usuario
     });
 }
 
