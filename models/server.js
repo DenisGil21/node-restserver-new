35a8321efbed7cddcc668d048efc0e0d99a62c19
Denis Gil <denisgilsil.21@gmail.com>
2021-04-11 11:44:37 -0500

    file upload agregado

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const { dbConnection } = require('../database/config');
 
 class Server {
@@ -13,6 +14,7 @@ class Server {
             buscar: '/api/buscar',
             categorias: '/api/categorias',
             productos: '/api/productos',
+            uploads: '/api/uploads',
             usuarios: '/api/usuarios',
         };
 
@@ -39,14 +41,22 @@ class Server {
 
         // Directorio público
         this.app.use(express.static('public'));
+
+        // FileUpload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes() {
         this.app.use(this.paths.auht, require('../routes/auth'));
         this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
-        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
+        this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
     listen() {
